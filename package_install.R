af2ea2226deeab0316e85fa2d372bcef1bc532ac
Stephen Elliott <sge2@princeton.edu>
2024-04-15 11:36:45 -0400

    package install

---
+++ package_install.R
@@ -0,0 +1,5 @@
+# Installs some of the necessary packages for running MVA-plots code
+install.packages('ggplot2')
+install.packages('GGally')
+install.packages('scales')
+install.packages('ggrepel')
